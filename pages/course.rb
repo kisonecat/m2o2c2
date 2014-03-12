6a81a9f6ee24ddbd5ace612ba07ae3552372b1a9
Jim Fowler <fowler@math.osu.edu>
2014-03-12 09:41:32 -0400

    Some course pages for the corresponding Coursera site

---
+++ pages/course.rb
@@ -0,0 +1,125 @@
+require 'rubygems'
+require 'json'
+require 'date'
+
+COURSE_NAME="m2o2c2-001"
+WEEK_COUNT = 6
+COURSE_START = Date.civil(2014, 3, 14)
+
+MAX_ATTEMPTS_PER_HOMEWORK = 999
+POINTS_PER_HOMEWORK = 10
+EXAM_MAX_ATTEMPTS="twice"
+EXAM_POINTS = 100
+CERTIFICATE_PERCENTAGE = 70
+CERTIFICATE_WITH_DISTINCTION_PERCENTAGE = 90
+TOTAL_POINTS = POINTS_PER_HOMEWORK * WEEK_COUNT + EXAM_POINTS
+
+WEEK_STARTS = (0...WEEK_COUNT).collect{ |i| COURSE_START + 7*i }
+WEEK_ENDS = (0...WEEK_COUNT).collect{ |i| COURSE_START + 7 + 7*i }
+
+COURSERA_COURSE='m2o2c2-001'
+
+def wikipedia(article,title)
+  html =<<EOF
+<a href="http://en.wikipedia.org/wiki/#{article}"><i class="icon-external-link"></i>&nbsp;#{title}</a>
+EOF
+
+  return html.strip
+end
+
+def linkto_week(week)
+  return '#' if week <= 0 or week > WEEK_COUNT
+
+  return "/#{COURSE_NAME}/wiki/view?page=week#{week}"
+end
+
+#$videos = JSON.parse(File.open("../identifiers/videos.json").read)
+#def linkto_video(video,title)
+#  raise "Missing video #{video}" unless $videos.keys.include?(video)##
+#
+#  html = <<EOF
+#<a href="/#{COURSE_NAME}/lecture/#{$videos[video]}"><i class="icon-film"></i>&nbsp;#{title}</a>
+#EOF
+
+#  return html.strip
+#end
+
+#$textbook = "https://d396qusza40orc.cloudfront.net/sequence%2Ftextbook%2Fsequences-and-series.pdf"
+#$labels = {}
+#for line in File.open('../textbook/textbook.aux').readlines
+#  if line.match( /^\\newlabel\{([^\}]+)\}\{\{([^\}]+)\}\{([^\}]+)\}/ )
+#    $labels[$1] = [$2, $3]
+#  end
+#  if line.match( /^\\newlabel\{([^\}]+)\}\{\{([^\}]+\{[^\}]+\})\}\{([^\}]+)\}/ )
+#    $labels[$1] = [$2.gsub( '{', '' ).gsub( '}', '' ), $3]
+#  end
+#end
+
+def linkto_textbook(label)
+  raise "Missing label #{label}" unless $labels.keys.include?(label)
+
+  kinds = {}
+  kinds['fig'] = 'Figure'
+  kinds['section'] = 'Section'
+  kinds['chapter'] = 'Chapter'
+  kinds['subsection'] = 'Subsection'
+  kinds['thm'] = 'Theorem'
+  kinds['example'] = 'Example'
+
+  kind_code = label.split(":")[0]
+  kind = kinds[kind_code]
+  name = $labels[label][0]
+  page = $labels[label][1]
+  link = "#{kind} #{name} on Page #{page} of the Textbook"
+
+  html = <<EOF
+<a href="#{$textbook}#page=#{page}"><i class="icon-book"></i>&nbsp;#{link}</a>
+EOF
+
+  return html.strip
+end
+
+def breadcrumbs(title)
+  return <<EOF
+<ul class="breadcrumb">
+  <li><a href="/#{COURSE_NAME}/class/index">Home</a> <span class="divider">/</span></li>
+  <li class="active">#{title}</li>
+</ul>
+EOF
+end
+
+def week_breadcrumbs(week)
+  return <<EOF
+<ul class="breadcrumb">
+  <li><a href="/#{COURSE_NAME}/class/index">Home</a> <span class="divider">/</span></li>
+  <li><a href="/#{COURSE_NAME}/wiki/view?page=syllabus">Syllabus</a> <span class="divider">/</span></li>
+  <li class="active">Week #{week}</li>
+</ul>
+EOF
+end
+
+def week_title(number,title)
+  title =<<EOF
+<h3><span style="float: right; color: #888; font-size: 60%; font-weight: normal;">#{WEEK_STARTS[number-1].strftime('%e %B %Y').strip}&ndash;#{WEEK_ENDS[number-1].strftime('%e %B %Y').strip}</span>
+<span id=\"week-title\">#{title}</span>
+</h3>
+EOF
+
+  return week_breadcrumbs(number) + title
+end
+
+
+def week_pager(week)
+  previous_disabled = ''
+  previous_disabled = 'disabled' if week == 1
+
+  next_disabled = ''
+  next_disabled = 'disabled' if week == WEEK_COUNT
+
+  return <<EOF
+  <ul class="pager">
+     <li class="previous #{previous_disabled}"><a href="#{linkto_week(week-1)}">&larr; Previous Week</a></li>
+     <li class="next #{next_disabled}"><a href="#{linkto_week(week+1)}">Next Week &rarr;</a></li>
+  </ul>
+EOF
+end
---
+++ pages/upload-page.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+#
+# download-pages.sh - Download all pages from a Coursera course to HTML files
+# 
+# (c) 2013, Jim Fowler.
+#
+
+# session and csrf are stored in the firefox cookie jar
+COOKIE_JAR=~/.mozilla/firefox/8b22l8b2.default/cookies.sqlite
+
+# the course that I am uploading to
+COURSERA_COURSE=m2o2c2-001
+
+# load the cookies from the jar
+COOKIES=$(sqlite3 $COOKIE_JAR 'select name, value from moz_cookies where baseDomain="coursera.org" and path="/'$COURSERA_COURSE'";' | tr '|' '=' | tr '\n' ';' | sed 's/;/; /g')
+MORE_COOKIES=$(sqlite3 $COOKIE_JAR 'select name, value from moz_cookies where baseDomain="coursera.org";' | tr '|' '=' | tr '\n' ';' | sed 's/;/; /g')
+
+CSRF_TOKEN=$(sqlite3 $COOKIE_JAR 'select value from moz_cookies where baseDomain="coursera.org" and path="/'$COURSERA_COURSE'" and name="csrf_token";' | tr '|' '=' | tr -d '\n' | sed 's/;/; /g')
+
+PAGE=$1
+
+PAGE_CODE=$(curl -L -e https://class.coursera.org/$COURSERA_COURSE/wiki/all --cookie "$COOKIES $MORE_COOKIES" https://class.coursera.org/m2o2c2-001/wiki/$PAGE | grep /admin/coursepages/ | sed 's/.*\/admin\/coursepages\///;s/".*//')
+
+CSRF2_COOKIE=$(echo $COOKIES $MORE_COOKIES | tr ';' '\n' | grep -m1 csrf2 | sed 's/^ //g' | cut -f 1 -d '=')
+CSRF2_TOKEN=$(echo $COOKIES $MORE_COOKIES | tr ';' '\n' | grep -m1 csrf2 | sed 's/^ //g' | cut -f 2 -d '=')
+
+cat $PAGE.html | python -c "import json,sys; print('{\"content\":' + json.dumps(sys.stdin.read())+\"}\")" | curl --cookie "$COOKIES $MORE_COOKIES" -H "Host: class.coursera.org" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:26.0) Gecko/20100101 Firefox/26.0" -H "Content-Type: application/json; charset=utf-8" -H "If-Match: 44" -H "X-HTTP-Method-Override: PATCH" -H "X-CSRF-Token:$CSRF_TOKEN" -H "X-CSRF2-Cookie: $CSRF2_COOKIE" -H "X-CSRF2-Token: $CSRF2_TOKEN" -H "X-Requested-With: XMLHttpRequest" -H "Referer: https://class.coursera.org/m2o2c2-001/admin/coursepages/$PAGE_CODE"  --data @- "https://class.coursera.org/$COURSERA_COURSE/admin/api/pages/$PAGE_CODE?fields=content"
